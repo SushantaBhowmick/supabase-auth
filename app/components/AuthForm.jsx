67ba4abec8a99737b97f982cfe4627958b2ed9d1
Sushanta Bhowmick <bhosushanta922@gmail.com>
2024-02-23 18:50:33 +0530

    update

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -1,31 +1,30 @@
-'use client'
+"use client";
 
-import React from 'react'
-import {Auth} from '@supabase/auth-ui-react'
-import {createClientComponentClient} from '@supabase/auth-helpers-nextjs'
+import React from "react";
+import { Auth } from "@supabase/auth-ui-react";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const AuthForm = () => {
-
-    const supabase = createClientComponentClient();
+  const supabase = createClientComponentClient();
 
   return (
-   <Auth
-    supabaseClient={supabase}
-    view='magic_link'
-    showLinks={false}
-    providers={[]}
-    redirectTo='https://supabase-auth-tau.vercel.app/auth/callback'
-    appearance={{
-        theme:'dark',
-        button:{
-            className:'bg-white-400 text-gray-900 hover:bg-gray-600'
+    <Auth
+      supabaseClient={supabase}
+      view="magic_link"
+      showLinks={false}
+      providers={[]}
+      redirectTo="https://supabase-auth-tau.vercel.app/auth/callback"
+      appearance={{
+        theme: "dark",
+        button: {
+          className: "bg-white-400 text-gray-900 hover:bg-gray-600",
         },
-        input:{
-            className:'bg-gray-700 border-gray-600 text-white'
-        }
-    }}
-   />
-  )
-}
+        input: {
+          className: "bg-gray-700 border-gray-600 text-white",
+        },
+      }}
+    />
+  );
+};
 
-export default AuthForm
+export default AuthForm;
